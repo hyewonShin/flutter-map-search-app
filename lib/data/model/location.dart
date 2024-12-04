2d6c021043b28d5b6685bdf73768bc7d79ebc10f
hyewonShin <67300338+hyewonShin@users.noreply.github.com>
2024-12-04 21:30:35 +0900

    feat: Location 모델 생성

---
+++ lib/data/model/location.dart
@@ -0,0 +1,38 @@
+// {
+//       "title": "<b>강남</b>구청",
+//       "link": "http://www.gangnam.go.kr/",
+//       "category": "공공,사회기관>구청",
+//       "description": "",
+//       "telephone": "",
+//       "address": "서울특별시 강남구 삼성동 16-1 강남구청",
+//       "roadAddress": "서울특별시 강남구 학동로 426 강남구청",
+//       "mapx": "1270475020",
+//       "mapy": "375173050"
+// },
+
+class Location {
+  String title;
+  String category;
+  String roadAddress;
+
+  Location({
+    required this.title,
+    required this.category,
+    required this.roadAddress,
+  });
+
+  Location.fromJson(Map<String, dynamic> map)
+      : this(
+          title: map['title'],
+          category: map['category'],
+          roadAddress: map['roadAddress'],
+        );
+
+  Map<String, dynamic> toJson() {
+    return {
+      'title': title,
+      'category': category,
+      'roadAddress': roadAddress,
+    };
+  }
+}
