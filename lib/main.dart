499ea44f93e9de98bdfc55e3d2d7038c4c82c7df
hyewonShin <67300338+hyewonShin@users.noreply.github.com>
2024-12-04 20:25:02 +0900

    feat: HomePage() UI 제작

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,9 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: HomePage());
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
   }
 }
--- lib/ui/pages/home/home_page.dart
+++ lib/ui/pages/home/home_page.dart
@@ -4,7 +4,54 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('HomePage'),
+      appBar: AppBar(
+        title: TextField(
+          decoration: InputDecoration(
+            hintText: '삼성동',
+            contentPadding: EdgeInsets.symmetric(vertical: 15, horizontal: 13),
+            enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(8.0),
+              borderSide: BorderSide(
+                color: Colors.grey,
+              ),
+            ),
+          ),
+        ),
+      ),
+      body: Expanded(
+        child: ListView.separated(
+          padding: EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+          separatorBuilder: (context, index) => SizedBox(height: 20),
+          itemCount: 10,
+          itemBuilder: (BuildContext context, int index) {
+            return item();
+          },
+        ),
+      ),
+    );
+  }
+
+  Container item() {
+    return Container(
+      padding: EdgeInsets.all(20),
+      width: double.infinity,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        border: Border.all(color: Colors.grey[300]!),
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            '삼성 1동 주민센터',
+            style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+          ),
+          Text('공공,사회기관>행정복지센터'),
+          Text('서울특별시 강남구 봉은사로 616 삼성1동 주민센터'),
+        ],
+      ),
     );
   }
 }
