5e1bc21b2d3bb2f08661b20eb19c522ea0fb6b09
hyewonShin <67300338+hyewonShin@users.noreply.github.com>
2024-12-05 21:38:10 +0900

    feat: actions 속성에 Icons.gps_fixed 아이콘 추가

--- lib/ui/pages/home/home_page.dart
+++ lib/ui/pages/home/home_page.dart
@@ -11,55 +11,71 @@ class HomePage extends StatelessWidget {
       onTap: () {
         FocusScope.of(context).unfocus();
       },
-      child: Scaffold(appBar: AppBar(
-        title: Consumer(builder: (context, ref, child) {
-          return TextField(
-            onSubmitted: (value) {
-              if (value.trim().isNotEmpty) {
-                final viewModel = ref.read(homeViewModel.notifier);
-                viewModel.searchMap(value);
-              }
-            },
-            decoration: InputDecoration(
-              hintText: '입력해주세요',
-              contentPadding:
-                  EdgeInsets.symmetric(vertical: 15, horizontal: 13),
-              enabledBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(8.0),
-                borderSide: BorderSide(
-                  color: Colors.grey,
-                ),
-              ),
-              focusedBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(8.0),
-                borderSide: const BorderSide(color: Colors.black),
-              ),
-            ),
-          );
-        }),
-      ), body: Consumer(builder: (context, ref, child) {
-        final addresses = ref.watch(homeViewModel);
-        return ListView.separated(
-          padding: EdgeInsets.symmetric(vertical: 20, horizontal: 20),
-          separatorBuilder: (context, index) => SizedBox(height: 20),
-          itemCount: addresses.length,
-          itemBuilder: (BuildContext context, int index) {
-            final data = addresses[index];
-            return GestureDetector(
-                onTap: () {
-                  Navigator.push(context, MaterialPageRoute(builder: (context) {
-                    String? validLink = data.link.length >= 5 &&
-                            data.link.substring(0, 5) == "https"
-                        ? data.link
-                        : null;
-
-                    return HomeDetailPage(validLink);
-                  }));
+      child: Scaffold(
+          appBar: AppBar(
+            title: Consumer(builder: (context, ref, child) {
+              return TextField(
+                onSubmitted: (value) {
+                  if (value.trim().isNotEmpty) {
+                    final viewModel = ref.read(homeViewModel.notifier);
+                    viewModel.searchMap(value);
+                  }
                 },
-                child: item(data));
-          },
-        );
-      })),
+                decoration: InputDecoration(
+                  hintText: '입력해주세요',
+                  contentPadding:
+                      EdgeInsets.symmetric(vertical: 15, horizontal: 13),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8.0),
+                    borderSide: BorderSide(
+                      color: Colors.grey,
+                    ),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8.0),
+                    borderSide: const BorderSide(color: Colors.black),
+                  ),
+                ),
+              );
+            }),
+            actions: [
+              GestureDetector(
+                onTap: () {
+                  print('위경도 아이콘 클릭');
+                },
+                child: Container(
+                  width: 50,
+                  height: 50,
+                  color: Colors.transparent,
+                  child: Icon(Icons.gps_fixed),
+                ),
+              )
+            ],
+          ),
+          body: Consumer(builder: (context, ref, child) {
+            final addresses = ref.watch(homeViewModel);
+            return ListView.separated(
+              padding: EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+              separatorBuilder: (context, index) => SizedBox(height: 20),
+              itemCount: addresses.length,
+              itemBuilder: (BuildContext context, int index) {
+                final data = addresses[index];
+                return GestureDetector(
+                    onTap: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) {
+                        String? validLink = data.link.length >= 5 &&
+                                data.link.substring(0, 5) == "https"
+                            ? data.link
+                            : null;
+
+                        return HomeDetailPage(validLink);
+                      }));
+                    },
+                    child: item(data));
+              },
+            );
+          })),
     );
   }
 }
