ce7ed2d8200e7df45e187de52aa126406ed0b9cf
hyewonShin <67300338+hyewonShin@users.noreply.github.com>
2024-12-09 20:56:59 +0900

    feat: HomeDetailPage 클래스 생성

--- lib/ui/pages/home_detail/home_detail_page.dart
+++ lib/ui/pages/home_detail/home_detail_page.dart
@@ -1,33 +1,64 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+import 'package:flutter_map_search_app/ui/pages/home_detail/widgets/naver_map.dart';
 
 class HomeDetailPage extends StatelessWidget {
   final link;
+  final title;
+  final lat;
+  final lon;
 
-  HomeDetailPage(this.link);
+  HomeDetailPage({
+    required this.link,
+    required this.title,
+    required this.lat,
+    required this.lon,
+  });
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text('HomeDetailPage'),
+          title: Text(title),
         ),
-        body: link != null
-            ? InAppWebView(
-                initialSettings: InAppWebViewSettings(
-                  mediaPlaybackRequiresUserGesture: true,
-                  javaScriptEnabled: true,
-                  userAgent:
-                      'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36',
-                ),
-                initialUrlRequest: URLRequest(
-                  url: WebUri(link),
-                ),
-              )
-            : Center(
-                child: Text(
-                '사이트가 존재하지 않습니다!',
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-              )));
+        body: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(children: [
+            Expanded(
+              flex: 1,
+              child: NaverMapApp(lat: lat, lon: lon),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Expanded(
+              flex: 1,
+              child: link != null
+                  ? InAppWebView(
+                      initialSettings: InAppWebViewSettings(
+                        mediaPlaybackRequiresUserGesture: true,
+                        javaScriptEnabled: true,
+                        userAgent:
+                            'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36',
+                      ),
+                      initialUrlRequest: URLRequest(
+                        url: WebUri(link),
+                      ),
+                    )
+                  : Container(
+                      decoration: BoxDecoration(color: Colors.white),
+                      child: Center(
+                          child: Text(
+                        '사이트가 존재하지 않습니다 😂',
+                        style: TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.bold),
+                      )),
+                    ),
+            ),
+            SizedBox(
+              height: 20,
+            )
+          ]),
+        ));
   }
 }
